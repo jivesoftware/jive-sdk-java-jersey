fe5ae8e9bd32d0cc351b45485320f9436b3c7999
Jussi Heikkola <jussi.heikkola@jivesoftware.com>
2014-02-26 09:50:40 -0800

    ignore unknown fields when deserializing json

--- jive-sdk/src/main/java/com/jivesoftware/sdk/api/tile/data/ActivityActor.java
+++ jive-sdk/src/main/java/com/jivesoftware/sdk/api/tile/data/ActivityActor.java
@@ -1,11 +1,13 @@
 package com.jivesoftware.sdk.api.tile.data;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 /**
  * Tile activity actor data
  */
 @JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ActivityActor {
     private String name;
     private String email;
