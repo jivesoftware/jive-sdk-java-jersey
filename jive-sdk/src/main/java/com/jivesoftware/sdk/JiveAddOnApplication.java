6b3a4d23e05ac3fedefea528f50dee8b42cf7c3a
Ryan Rutan <ryan.rutan@jivesoftware.com>
2014-08-14 08:52:15 -0500

    update

--- jive-sdk/src/main/java/com/jivesoftware/sdk/JiveAddOnApplication.java
+++ jive-sdk/src/main/java/com/jivesoftware/sdk/JiveAddOnApplication.java
@@ -18,38 +18,12 @@
 
 package com.jivesoftware.sdk;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import com.jivesoftware.sdk.api.entity.*;
-import com.jivesoftware.sdk.api.entity.impl.memory.*;
-import com.jivesoftware.sdk.client.JiveAPIClient;
-import com.jivesoftware.sdk.client.JiveAnalyticsClient;
-import com.jivesoftware.sdk.client.JiveTileClient;
-import com.jivesoftware.sdk.client.oauth.JiveOAuthClient;
-import com.jivesoftware.sdk.client.oauth.JiveOAuthEventListener;
-import com.jivesoftware.sdk.service.filter.*;
-import com.jivesoftware.sdk.service.health.HealthService;
-import com.jivesoftware.sdk.service.instance.InstanceService;
-import com.jivesoftware.sdk.service.oauth.JiveOAuth2Service;
-import com.jivesoftware.sdk.service.oauth.facebook.FacebookOAuth2Service;
-import com.jivesoftware.sdk.service.oauth.google.GoogleOAuth2Service;
-import com.jivesoftware.sdk.service.oauth.twitter.TwitterOAuth1Service;
-import com.jivesoftware.sdk.service.storage.StorageService;
-import com.jivesoftware.sdk.service.tile.TileService;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.jboss.weld.Weld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Singleton;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
 
 /**
  * Created by rrutan on 1/29/14.
