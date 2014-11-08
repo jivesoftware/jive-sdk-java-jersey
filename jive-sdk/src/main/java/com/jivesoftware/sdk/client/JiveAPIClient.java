3768d9fa09c54e8e8c047e46021e6b889b32dc57
Ryan Rutan <ryan.rutan@jivesoftware.com>
2014-11-08 07:55:18 -0600

    updated

--- jive-sdk/src/main/java/com/jivesoftware/sdk/client/JiveAPIClient.java
+++ jive-sdk/src/main/java/com/jivesoftware/sdk/client/JiveAPIClient.java
@@ -94,13 +94,13 @@ public class JiveAPIClient extends BaseJiveClient {
             return response;
         } catch (BadRequestException bre) {
             log.error("Error Calling API [" + uri + "]", bre);
-            throw JiveClientException.buildException("Error Pushing Data to Tile [" + uri + "]",bre,null,data,data.getClass());
+            throw JiveClientException.buildException("Error Calling API [" + uri + "]",bre,null,data,data.getClass());
         } catch (InterruptedException ie) {
             log.error("Error Calling API [" + uri + "]", ie);
-            throw JiveClientException.buildException("Error Pushing Data to Tile [" + uri + "]",ie,null,data,data.getClass());
+            throw JiveClientException.buildException("Error Calling API [" + uri + "]",ie,null,data,data.getClass());
         } catch (ExecutionException ee) {
             log.error("Error Calling API [" + uri + "]", ee);
-            throw JiveClientException.buildException("Error Pushing Data to Tile [" + uri + "]",ee,null,data,data.getClass());
+            throw JiveClientException.buildException("Error Calling API [" + uri + "]",ee,null,data,data.getClass());
         } // end try/catch
 
     } // end call
